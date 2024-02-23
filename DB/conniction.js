0148669232c60da4efa2d9e154e4d7f17c15b1ca
roaa-kittaneh <roaakittaneh2004@gmail.com>
2024-02-23 16:34:01 +0300

    change database config

--- DB/conniction.js
+++ DB/conniction.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize';
 
- export const sequelize = new Sequelize('blog', 'root', '', {
-    host: 'localhost',
+ export const sequelize = new Sequelize('freedb_blog-user', 'freedb_roaaa', '3fycRvbW?kfF9$M', {
+    host: 'sql.freedb.tech',
+    port: 3306,
     dialect: 'mysql'/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
   });
 
